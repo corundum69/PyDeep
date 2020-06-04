91ed49e7d6da1ba9cf7a9529782af22033ffe17e
corundum69 <51014464+corundum69@users.noreply.github.com>
2020-06-04 18:54:34 +0900

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,19 @@
+## BeautifulSoup 기본 사용법 (bs-test1.py)
+from bs4 import BeautifulSoup
+
+html = """
+<html><body>
+    <h1>스크레이핑이란?</h1>
+    <p>웹페이지를 분석하는 것</p>
+    <p>원하는 부분을 추출하는 것</p>
+</body></html>
+"""
+soup = BeautifulSoup(html, 'html.parser')
+
+h1 = soup.html.body.h1
+p1 = soup.html.body.p
+p2 = p1.next_sibling.next_sibling
+
+print("h1 = " + h1.string)
+print("p  = " + p1.string)
+print("p  = " + p2.string)
